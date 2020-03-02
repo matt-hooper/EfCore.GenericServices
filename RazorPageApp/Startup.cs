436efd6cf5c7d96f3cc5c85c0f5f2026ea6c7069
Matthew Hooper <mhooper@scottlogic.com>
2020-03-02 20:58:05 +0000

    get working with 3.1 and fix a couple of issues

--- RazorPageApp/Startup.cs
+++ RazorPageApp/Startup.cs
@@ -1,16 +1,19 @@
-using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 using DataLayer.EfCode;
-using GenericServices.Setup;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
 using ServiceLayer.HomeController.Dtos;
+using GenericServices.Setup;
 
 namespace RazorPageApp
 {
@@ -26,11 +29,7 @@ namespace RazorPageApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // thanks to https://exceptionnotfound.net/setting-a-custom-default-page-in-asp-net-core-razor-pages/
-            services.AddMvc().AddRazorPagesOptions(options =>
-            {
-                options.Conventions.AddPageRoute("/Home/Index", "");
-            });
+            services.AddRazorPages();
 
             //--------------------------------------------------------------------
             //var connection = Configuration.GetConnectionString("DefaultConnection");
@@ -43,27 +42,10 @@ namespace RazorPageApp
             //--------------------------------------------------------------------
 
             services.GenericServicesSimpleSetup<EfCoreContext>(Assembly.GetAssembly(typeof(BookListDto)));
-
-            //This is the version you would use if you were registering multiple DbContext
-            //services.ConfigureGenericServicesEntities(typeof(BookDbContext), typeof(OrderDbContext))
-            //    .ScanAssemblesForDtos(Assembly.GetAssembly(typeof(BookListDto)))
-            //    .RegisterGenericServices();
-
-            //--------------------------------------------------------------------------------------------
-            //I removed the code below, as this was only needed for the hand-coded versions of the services
-            ////Now I use AutoFac to do some of the more complex registering of services
-            //var containerBuilder = new ContainerBuilder();
-
-            ////Now I use the ServiceLayer AutoFac module that registers all the other DI items, such as my biz logic
-            //containerBuilder.RegisterModule(new ServiceLayerModule());
-
-            //containerBuilder.Populate(services);
-            //var container = containerBuilder.Build();
-            //return new AutofacServiceProvider(container);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -72,21 +54,21 @@ namespace RazorPageApp
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
-            // thanks to https://wildermuth.com/2017/11/19/ASP-NET-Core-2-0-and-the-End-of-Bower for this trick
-            if (env.IsDevelopment())
-            {
-                app.UseStaticFiles(new StaticFileOptions()
-                {
-                    FileProvider = new PhysicalFileProvider(
-                        Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
-                    RequestPath = new PathString("/vendor")
-                });
-            }
 
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
         }
     }
 }
